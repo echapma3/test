5f06e5c6773e205224c77ce819e7a90c7e269293
Ellen <echapma3@jaguarlandrover.com>
2020-06-01 09:46:11 +0100

    random

--- first.py
+++ first.py
@@ -1,3 +1,15 @@
 print("test the second time")
 
 print("hello")
+
+print("test the second time")
+
+print("hello")
+
+
+print("12345")
+
+number = 4
+
+if number > 0:
+    print('number is positive')
